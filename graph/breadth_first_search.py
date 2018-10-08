f724987525301f3a6539cb4b32983abc668d1284
Newman Thurairatnam <newman.joyal@gmail.com>
2018-10-08 15:59:17 -0400

    An implementation of breadth first search

--- graph/breadth_first_search.py
+++ graph/breadth_first_search.py
@@ -1,5 +1,24 @@
-def breadth_first_search(graph):
+def breadth_first_search(node):
+    to_return = {}
     visited = []
-    if graph is None:
-        return []
-    
+    level = 0
+    if node is None:
+        return to_return
+    graph_traverse(level, node, to_return, visited)
+    return to_return
+
+
+def graph_traverse(level, node, to_return, visited):
+    insert_node(level, node, to_return, visited)
+    for adj_node in node.adjacent:
+        if adj_node not in visited:
+            graph_traverse(level + 1, adj_node, to_return, visited)
+
+
+def insert_node(level, node, to_return, visited):
+    visited.append(node)
+    if to_return.get(level):
+        to_return.get(level).append(node)
+    else:
+        to_return.update({level: []})
+        to_return.get(level).append(node)
--- graph/test/test_breadth_first_search.py
+++ graph/test/test_breadth_first_search.py
@@ -1,19 +1,48 @@
 import unittest
+from graph.graph_node import Node
 from graph.breadth_first_search import breadth_first_search
 
 
 class BreadthFirstSearch(unittest.TestCase):
     def test_empty_graph(self):
         graph = None
-        self.assertEqual(breadth_first_search(graph), [])
+        self.assertEqual(breadth_first_search(graph), {})
 
     def test_simple_graph(self):
-        pass
-
+        node_1 = Node(1)
+        node_2 = Node(2)
+        node_3 = Node(3)
+        node_1.add_adjacent_nodes(node_2)
+        node_2.add_adjacent_nodes(node_3)
+        depth_first = breadth_first_search(node_1)
+        self.assertEqual(depth_first.get(0), [node_1])
+        self.assertEqual(depth_first.get(1), [node_2])
+        self.assertEqual(depth_first.get(2), [node_3])
 
     def test_graph_with_cycle(self):
-        pass
+        node_1 = Node(1)
+        node_2 = Node(2)
+        node_3 = Node(3)
+        node_1.add_adjacent_nodes(node_2)
+        node_2.add_adjacent_nodes(node_3)
+        node_3.add_adjacent_nodes(node_1)
+        depth_first = breadth_first_search(node_1)
+        self.assertEqual(depth_first.get(0), [node_1])
+        self.assertEqual(depth_first.get(1), [node_2])
+        self.assertEqual(depth_first.get(2), [node_3])
 
-
-    def test_large_graph_wit_cycle(self):
-        pass
+    def test_large_graph_with_cycle(self):
+        node_0 = Node(0)
+        node_1 = Node(1)
+        node_2 = Node(2)
+        node_3 = Node(3)
+        node_2.add_adjacent_nodes(node_0)
+        node_2.add_adjacent_nodes(node_3)
+        node_0.add_adjacent_nodes(node_1)
+        node_0.add_adjacent_nodes(node_2)
+        node_1.add_adjacent_nodes(node_2)
+        node_3.add_adjacent_nodes(node_3)
+        depth_first = breadth_first_search(node_2)
+        self.assertEqual(depth_first.get(0), [node_2])
+        self.assertEqual(depth_first.get(1), [node_0, node_3])
+        self.assertEqual(depth_first.get(2), [node_1])
